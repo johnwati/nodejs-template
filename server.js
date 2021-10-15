0fc18b537ccde54005c8adb3ce2539bd812db06e
John Wati <jonseemeyou@gmail.com>
2021-10-15 12:31:44 +0300

    crud with mongo db

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const express = require("express");
 // const route = require("./routes/routes");
+const route = require("./app/routes/note.routes.js");
+
 
 const app = express();
 
@@ -10,8 +12,24 @@ app.use(cors({ origin: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//=============================setting database connection====================
+// Configuring the database
+const dbConfig = require('./app/config/mongodb.config.js');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+// Connecting to the database
+mongoose.connect(dbConfig.url, {
+	useNewUrlParser: true
+}).then(() => {
+    console.log("Successfully connected to the database");    
+}).catch(err => {
+    console.log('Could not connect to the database. Exiting now...', err);
+    process.exit();
+});
+//=============================end of database connection=================
 // routes middleware
 // app.use(route);
+require('./app/routes/note.routes.js')(app);
 
 // define a simple route
 app.get('/', (req, res) => {
